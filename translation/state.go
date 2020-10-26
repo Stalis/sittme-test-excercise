571bcf1209a8eb537a2ad77639e562f67e43d98f
Бабкин Станислав <stalis@Babkins-MacBook-Air.local>
2020-10-26 19:30:46 +0300

    Добавлена структура трансляции с механизмами изменением состояния

---
+++ translation/state.go
@@ -0,0 +1,15 @@
+package translation
+
+// State Тип состояния трансляции
+type State int
+
+const (
+	// Created Трансляция создана
+	Created State = iota
+	// Active Трансляция активна
+	Active
+	// Interrupted Трансляция прервана
+	Interrupted
+	// Finished Трансляция завершена
+	Finished
+)
